cc81b0258cf64234f51222a202d1ac0ae986f86a
Vadim Valuev <gease@mail.ru>
2017-11-24 13:14:38 +0000

    Moved page layout into template.

--- src/Controller/EjikznaykaArithmeticsController.php
+++ src/Controller/EjikznaykaArithmeticsController.php
@@ -10,25 +10,8 @@ class EjikznaykaArithmeticsController extends ControllerBase {
     $config = \Drupal::config('ejikznayka.arithmetics')->get();
     return [
       '#type' => 'markup',
-      '#markup' => '<input type="button" name="start" id="start" value="Начать" />
-                    <div id="show"></div>
-                    <div id="check">
-                      <input type="button" name="show_answer" id="show_answer" value="Показать ответ" /><br>
-                      <div id="answer">
-                        Правильный ответ: <div id="answer_placeholder"> </div>
-                      </div>
-                      <input type="text" name="input_answer" id="input_answer" placeholder="Ваш ответ" />
-                      <input type="button" name="check_answer" id="check_answer" value="Проверить ответ" />
-                      <div id="check_answer_message">
-                        <div class="incorrect">Неверно!</div>
-                        <div class="correct">Верно!</div> 
-                      </div>
-                    </div>',
-      '#allowed_tags' => ['div', 'input'],
+      '#theme' => 'ejikznayka_arithmetics',
       '#attached' => [
-        'library' => [
-          'ejikznayka_arithmetics/ejikznayka_arithmetics',
-        ],
         'drupalSettings' => [
           'ejikznayka' => [
             'arithmetics' => [
